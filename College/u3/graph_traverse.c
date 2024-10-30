26470cac961c9e89b04c39b9a589aca5d476bd04
devanshishukla17 <shukla.17.devanshi@gmail.com>
2024-10-30 15:38:34 +0530

    new files

--- College/u3/graph_traverse.c
+++ College/u3/graph_traverse.c
@@ -5,3 +5,96 @@ GRAPH TRAVERSAL-->
 ~DEPTH-FIRST SEARCH-->DFS-->STACK 
 
 */
+
+#include<stdio.h>
+//#include "4.Graph_Adj_list.c"
+//#include "5.Graph_Adj_mat.c"
+#include "graph.c"
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} NODE;
+
+int f=-1,r=-1;
+int isempty(int *q){
+    return (f==r && f==-1);
+}
+void enqueue(int *q,int data){
+    if(isempty(q))
+        f++;
+    q[++r]=data;
+}
+int dequeue(int *q){
+    int d=q[f];
+    if(f==r && f!=-1){//one element condition
+        f=r=-1;
+    }
+    else
+        f++;
+    return d;
+}
+
+void bfs_AdjMat(GRAPH *g)
+ {
+     int source,*queue,*visited,v,i;
+     printf("\nEnter the source vertex");
+     scanf("%d",&source);
+     v=g->vertex;
+     queue=(int*)calloc(v,sizeof(int));
+     visited=(int*)calloc(v+1,sizeof(int));
+     enqueue(queue,source);
+     visited[source]=1;
+     printf("\n%d ",source);
+     while(!isempty(queue)){
+         i=dequeue(queue);
+         for(int j=1;j<=v;j++){
+            if(g->adjmat[i][j]==1 && visited[j]==0){
+                enqueue(queue,j);
+                visited[j]=1;
+                printf("%d ",j);
+            }
+         }
+     }
+ }
+ //BFS implementation using Adjacency List
+ void bfs_AjdList(NODE *l)
+ {
+     int source,*queue,*visited,v,i,j;
+     NODE *t;
+     printf("\nEnter the source vertex");
+     scanf("%d",&source);
+     v=l[0].data;
+     queue=(int*)calloc(v,sizeof(int));
+     visited=(int*)calloc(v+1,sizeof(int));
+     enqueue(queue,source);
+     visited[source]=1;
+     printf("\n%d ",source);
+     while(!isempty(queue))
+     {
+         i=dequeue(queue);
+         t=&l[i];
+         while(t->next!=NULL)
+         {
+            t=t->next;
+            j=t->data;
+            if(visited[j]==0){
+                enqueue(queue,j);
+                visited[j]=1;
+                printf("%d ",j);
+            }
+         }
+     }
+ }
+
+void main(){
+    GRAPH g;
+    create_adjMatrix(&g);
+    matrixDisplay(g);
+    bfs_AdjMat(&g);
+    NODE graph[MAX];
+    create_adjList(graph);
+    ListDisplay(graph);
+    bfs_AjdList(graph);
+}
